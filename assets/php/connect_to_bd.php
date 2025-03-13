400466eb46fc728a173245f8de502daf2ab2bf7e
ekanga <ekangay18@gmail.com>
2025-03-13 13:43:55 +0100

    11commits

--- assets/php/connect_to_bd.php
+++ assets/php/connect_to_bd.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "127.0.0.1" ;
-$dbname = "ecommerce";
-$username = "root" ;
-$password = "";
+$servername = "mysql-yann-dev..alwaysdata.net" ;
+$dbname = "yann-dev_ecommerce1";
+$username = "yann-dev" ;
+$password = "eya-dev";
 try {
     $conn =new PDO("mysql:host=$servername;dbname=$dbname",$username,$password) ;
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION) ;
